46f3b1a103e8709fcbcf77a9f054db648483fc5b
jing sun <jing.sun@rea-group.com>
2016-03-14 20:03:14 +0800

    df

--- buildtasks/tests/static_security_scanner.sh
+++ buildtasks/tests/static_security_scanner.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 set -ex
-if [[ "$STATIC_ANALYSIS_ENV" != 'bamboo-build' ]]; then
-   git stash -q --keep-index
-fi
-dawn -s -z .
+dawn -z .
 RESULT=$?
 echo $RESULT
-if [[ "$STATIC_ANALYSIS_ENV" != 'bamboo-build' ]]; then
-   git stash pop -q
-fi
 [ $RESULT -ne 0 ] && exit 1
 exit 0
