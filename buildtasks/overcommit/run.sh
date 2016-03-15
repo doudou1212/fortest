ca3842b445e3c341cc953c24e8f0cdf7777fd9e3
jing sun <jing.sun@rea-group.com>
2016-03-15 20:43:47 +0800

    nihao

--- buildtasks/overcommit/run.sh
+++
@@ -1,7 +0,0 @@
-source buildtasks/setup_ruby.sh
-bundle config build.nokogiri --use-system-libraries
-bundle install
-overcommit --install
-overcommit --sign || true
-overcommit --sign pre-commit
-overcommit --run
